bdcdb34ea7ed984675a60598e85b228ac1b337be
Robert <gamingfardsletsgo@gmail.com>
2024-06-26 22:38:45 -0400

    Added pseudo-code and html document to my project.

---
+++ rock-paper-scissors.js
@@ -0,0 +1,10 @@
+/*PSEUDOCODE
+
+1. use a loop to prompt user for choice
+2. pick a random number from 1 to 3 for the computer
+3. if player wins using rock-paper-scissors logic, add 1 point to their score
+4. else if computer wins, add 1 point to the computer's score
+5. display scores and continue loop
+
+*/
+
